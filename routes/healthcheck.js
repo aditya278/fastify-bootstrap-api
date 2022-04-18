0d8b5edd52bd1c9136801fc61a9fe1605291c101
Aditya <aditya.shukla278@gmail.com>
2022-04-18 08:13:59 +0530

    Add Swagger & update Healthcheck

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -1,11 +1,24 @@
-'use strict'
+'use strict';
 
 module.exports = async function (fastify, opts) {
   fastify.route({
     method: 'GET',
     url: '/healthcheck',
+    schema: {
+      tags: ['Healthcheck'],
+      description: 'Healthcheck to check if the server is running',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' },
+            timestamp: { type: 'string' },
+          },
+        },
+      },
+    },
     handler: async (request, reply) => {
-      return { status: 'ok' };
-    }
-  })
-}
+      return { status: 'ok', timestamp: new Date().toISOString() };
+    },
+  });
+};
